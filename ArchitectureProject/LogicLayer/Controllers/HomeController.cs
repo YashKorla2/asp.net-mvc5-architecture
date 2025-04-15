05bc42dacb5c478d845cb9940cd94764c6817f08
YashKorla2 <Yash.Korla2@encora.com>
2025-04-15 13:12:24 +0530

    Complete Project

---
+++ ArchitectureProject/LogicLayer/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using ServiceLayer.StudentService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace LogicLayer.Controllers
+{
+    public class HomeController : BaseController
+    {
+        #region Fields
+
+        private IStudentService _StudentService;
+
+        #endregion
+
+        #region Constructor
+
+        public HomeController(IStudentService StudentService)
+        {
+            _StudentService = StudentService;
+        }
+
+        #endregion
+
+        #region Actions
+        public ActionResult Index()
+        {
+            _StudentService.AddStudent();
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ ArchitectureProject/ServiceLayer/Student/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceLayer.UnitOfWorkService;
+
+namespace ServiceLayer.StudentService
+{
+    public class StudentService : IStudentService
+    {
+        private IUnitOfWork _uow;
+        public StudentService(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+        public int AddStudent()
+        {
+            var s = new DB.Core.Student();
+            s.Name = "Asad";
+            s.Batch = "BS101";
+            _uow.StudentRepo.Insert(s);
+           var data = _uow.StudentRepo.GetById(1);
+            data.Name = "Omar";
+            _uow.StudentRepo.Edit(data);
+            _uow.Commit();
+            return 0;
+        }
+    }
+}
