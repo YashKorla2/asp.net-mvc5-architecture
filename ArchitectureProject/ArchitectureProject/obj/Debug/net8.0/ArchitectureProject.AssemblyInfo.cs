88225b31d82327b549affc651c1f019ec98d91c0
NoReply Amazon <no-reply@amazon.com>
2025-04-22 07:42:06 +0000

    Revert "[Commit] Iteration 42 #errors = 5 <= 5."
    This reverts commit 07d63320f9c23efd4de31c6ed80ab5f2a65daee5.

--- ArchitectureProject/ArchitectureProject/obj/Debug/net8.0/ArchitectureProject.AssemblyInfo.cs
+++ ArchitectureProject/ArchitectureProject/obj/Debug/net8.0/ArchitectureProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArchitectureProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6ef23e885620cce35609cd006d8567ed03a5ee7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4627ea70eea170f14ef4e19b4d590e46ad9d09f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArchitectureProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArchitectureProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
