14843b8d0e7e876cf2be1847cfef33ffa6738080
NoReply Amazon <no-reply@amazon.com>
2025-04-22 08:23:06 +0000

    [Commit] Iteration 112 #errors = 8 <= 4.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/ArchitectureProject/DataLayer/Student.Data/IStudentRepo.cs][CS0246][11,49] The type or namespace name 'DB' could not be found (are you missing a using directive or an assembly reference?)``.

--- ArchitectureProject/DB.Core/obj/Debug/net8.0/DB.Core.AssemblyInfo.cs
+++ ArchitectureProject/DB.Core/obj/Debug/net8.0/DB.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DB.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e27e463166ba3cf5eabffd94867ca72d9d241272")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dee2eee5bbf300f30b4f67bea7cd07be99f746d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DB.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DB.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
