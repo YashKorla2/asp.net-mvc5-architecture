3e3da8f9f66d3c8f191ac8ba57e3b80f53bc9d6f
NoReply Amazon <no-reply@amazon.com>
2025-04-22 08:59:10 +0000

    Revert "[Commit] Iteration 174 #errors = 9 <= 4."
    This reverts commit 0523750d2c68b5246b6b5b3847688dadb9e5f144.

--- ArchitectureProject/DataLayer/obj/Debug/net8.0/DataLayer.AssemblyInfo.cs
+++ ArchitectureProject/DataLayer/obj/Debug/net8.0/DataLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a90dfc5514587cb73a3ffddcc56f3ec0ff7a0c3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+503493764cfc05f58f8e0c29d00c74acc9d6ece0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
