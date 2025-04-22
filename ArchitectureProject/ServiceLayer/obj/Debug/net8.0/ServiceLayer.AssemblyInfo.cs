f6680e7f3a8b92d860672316a0fc9a2d2cf2a928
NoReply Amazon <no-reply@amazon.com>
2025-04-22 08:35:15 +0000

    Revert "[Commit] Iteration 131 #errors = 5 <= 5."
    This reverts commit 4b3fa5e03492de536ac74eee07438346beb749f1.

--- ArchitectureProject/ServiceLayer/obj/Debug/net8.0/ServiceLayer.AssemblyInfo.cs
+++ ArchitectureProject/ServiceLayer/obj/Debug/net8.0/ServiceLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a38787e0b551b637475daf2d103afb99107c77a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32071235a80039e0aa56de074ef59dbbc53c9f92")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
