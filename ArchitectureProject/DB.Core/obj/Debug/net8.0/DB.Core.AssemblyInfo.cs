35823bcb87d8ca21b6ea067c6a9e1da3f3ae5e33
NoReply Amazon <no-reply@amazon.com>
2025-04-22 07:20:11 +0000

    Revert "[Commit] Iteration 8 #errors = 6 <= 6."
    This reverts commit bff30d027f0749256d397182541882e7830e4a7a.

--- ArchitectureProject/DB.Core/obj/Debug/net8.0/DB.Core.AssemblyInfo.cs
+++ ArchitectureProject/DB.Core/obj/Debug/net8.0/DB.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DB.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36aea051a570a9dbc3065a8b01a962ff88545916")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a44b932791be37a9b4fdc4deeb9bdd12739f7f58")]
 [assembly: System.Reflection.AssemblyProductAttribute("DB.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DB.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
