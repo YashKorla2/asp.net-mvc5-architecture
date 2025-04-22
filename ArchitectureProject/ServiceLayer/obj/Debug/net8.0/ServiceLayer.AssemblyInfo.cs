f929840e1e0d48b16d2f24791151880689897808
NoReply Amazon <no-reply@amazon.com>
2025-04-22 08:13:44 +0000

    [Commit] Iteration 97 #errors = 5 <= 5.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/ArchitectureProject/ServiceLayer/Student/Student.cs][CS1503][22,37] Argument 1: cannot convert from 'DB.Core.Student [/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/ArchitectureProject/DB.Core/Student.cs(15)]' to 'DB.Core.Student [DataLayer, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null]'``.

--- ArchitectureProject/ServiceLayer/obj/Debug/net8.0/ServiceLayer.AssemblyInfo.cs
+++ ArchitectureProject/ServiceLayer/obj/Debug/net8.0/ServiceLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2aeb3538abc2918b7c4cc0ed6d854e3fc4fa04a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d75bd8629cb58fab5b8ac5d1798c00f3391fcb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
