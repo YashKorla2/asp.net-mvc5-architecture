73f4cfc5d1859c2ad977d8480073b6be03289a0f
NoReply Amazon <no-reply@amazon.com>
2025-04-22 07:16:40 +0000

    [Commit] Iteration 4 #errors = 9 <= 9.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/ArchitectureProject/DataLayer/GenericRepository/Repository.cs][CS0246][-1,-1] The type or namespace name 'DB' could not be found (are you missing a using directive or an assembly reference?)``.

--- ArchitectureProject/ServiceLayer/obj/Debug/net8.0/ServiceLayer.AssemblyInfo.cs
+++ ArchitectureProject/ServiceLayer/obj/Debug/net8.0/ServiceLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05bc42dacb5c478d845cb9940cd94764c6817f08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d719597353f151e41afbf752b2ffec860cd39792")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
