b81d23cafabec2f5eeab0daa30accdf3bab01c40
NoReply Amazon <no-reply@amazon.com>
2025-04-22 08:14:40 +0000

    Revert "[Commit] Iteration 98 #errors = 5 <= 5."
    This reverts commit b4fa58ba92933de768835580d264f9a7752e81e8.

--- ArchitectureProject/ServiceLayer/Student/Student.cs
+++ ArchitectureProject/ServiceLayer/Student/Student.cs
@@ -1,10 +1,9 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ServiceLayer.UnitOfWorkService;
-using DB.Core;
 
 namespace ServiceLayer.StudentService
 {
