7d75bd8629cb58fab5b8ac5d1798c00f3391fcb5
NoReply Amazon <no-reply@amazon.com>
2025-04-22 08:09:39 +0000

    Revert "[Commit] Iteration 90 #errors = 5 <= 5."
    This reverts commit 606dd02b44c92d8963e6b1e17446146a14951056.

--- ArchitectureProject/DataLayer/Student.Data/StudentRepo.cs
+++ ArchitectureProject/DataLayer/Student.Data/StudentRepo.cs
@@ -12,6 +12,11 @@ namespace DataLayer.Student.Data
 {
     public class StudentRepo : Repository<DB.Core.Student>, IStudentRepo
     {
+        static StudentRepo()
+        {
+            Assembly.Load("DB.Core");
+        }
+
         public StudentRepo(DbContext context) : base(context)
         {
         }
