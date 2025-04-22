61c820514f047b4883cf63cc4f1497a4e93b5234
NoReply Amazon <no-reply@amazon.com>
2025-04-22 07:57:18 +0000

    [Commit] Iteration 68 #errors = 5 <= 5.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/ArchitectureProject/ServiceLayer/Student/Student.cs][CS1503][22,37] Argument 1: cannot convert from 'DB.Core.Student [/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/ArchitectureProject/DB.Core/Student.cs(15)]' to 'DB.Core.Student [DataLayer, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null]'``.

--- ArchitectureProject/BO/obj/Debug/net8.0/BO.AssemblyInfo.cs
+++ ArchitectureProject/BO/obj/Debug/net8.0/BO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4627ea70eea170f14ef4e19b4d590e46ad9d09f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19e2f941930e86aa3839bd775b30ed0452f2f9a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
