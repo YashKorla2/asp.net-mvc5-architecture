db0886cf8e4fc416daab55517851dedfda63a25c
NoReply Amazon <no-reply@amazon.com>
2025-04-22 08:39:34 +0000

    [Commit] Iteration 139 #errors = 5 <= 5.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/ArchitectureProject/DataLayer/Student.Data/StudentRepo.cs][CS0246][10,7] The type or namespace name 'DB' could not be found (are you missing a using directive or an assembly reference?)``.

--- ArchitectureProject/DataLayer/Student.Data/StudentRepo.cs
+++ ArchitectureProject/DataLayer/Student.Data/StudentRepo.cs
@@ -4,8 +4,6 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Reflection;
-using System.IO;
 using DataLayer.GenericRepository;
 using DB.Core;
 
@@ -13,13 +11,6 @@ namespace DataLayer.Student.Data
 {
     public class StudentRepo : Repository<DB.Core.Student>, IStudentRepo
     {
-        static StudentRepo()
-        {
-            string assemblyPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DB.Core.dll");
-            Assembly assembly = Assembly.LoadFrom(assemblyPath);
-            AppDomain.CurrentDomain.Load(assembly.GetName());
-        }
-
         public StudentRepo(DbContext context) : base(context)
         {
         }
